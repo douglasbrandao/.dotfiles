1f22e111eae040e73bce24f773f9329021ff5318
Douglas Brandão <douglasfrb@gmail.com>
2023-01-16 21:29:52 -0400

    feat: add install.sh create symlinks

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sfn /home/$USER/.dotfiles/git/.gitconfig ~/.
+ln -sfn /home/$USER/.dotfiles/zshrc/.zshrc ~/.
+ln -sfn /home/$USER/.dotfiles/vim/.vimrc ~/.
+ln -sfn /home/$USER/.dotfiles/nvim/ ~/.config
