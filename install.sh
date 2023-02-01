85e163a2357b049e9ecd112401f811f160cacf01
Douglas Brandão <douglasfrb@gmail.com>
2023-01-31 21:06:37 -0400

    chore: edit install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ distro_name=${strarr[1]}
 if [[ $distro_name == "Ubuntu" ]]; then
   source "$(dirname $0)/install-ubuntu.sh"
 elif [[ $distro_name == "Arch" ]]; then
-  echo "in progress"
+  source "$(dirname $0)/install-arch.sh"
 fi
