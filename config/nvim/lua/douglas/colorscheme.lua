0076e89833a403df942f11da8f91434f644c4780
Douglas Brandão <douglasfrb@gmail.com>
2023-11-06 12:00:58 -0300

    fix: colorscheme

--- config/nvim/lua/douglas/colorscheme.lua
+++ config/nvim/lua/douglas/colorscheme.lua
@@ -1,14 +1,12 @@
+local is_status_ok, _ = pcall(vim.cmd, "colorscheme tokyonight")
+
+if not is_status_ok then
+  vim.notify("colorscheme not found")
+  return
+end
+
 require("tokyonight").setup({
   style="night",
 })
-
-local colorscheme = "tokyonight"
-
-local is_status_ok, _ = pcall(vim.cmd, "colorscheme".. colorscheme)
-
-if not is_status_ok then
-  vim.notify("colorscheme" .. colorscheme .. "not found")
-  return
-end
 
 vim.cmd "colorscheme tokyonight"
