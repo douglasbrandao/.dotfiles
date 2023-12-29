92dcec1f6a52c7ebd2e3097440973ade8a2b0e85
Douglas Brandão <douglasfrb@gmail.com>
2023-12-28 22:27:32 -0300

    fix: add events

--- config/nvim/lua/douglas/linters.lua
+++ config/nvim/lua/douglas/linters.lua
@@ -8,7 +8,7 @@ lint.linters_by_ft = {
   typescript = { "eslint_d", },
 }
 
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
   callback = function()
     lint.try_lint()
   end,
