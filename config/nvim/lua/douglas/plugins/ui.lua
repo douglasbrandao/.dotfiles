8c67f3e9782b1364fe1c9a3adb510d8f5e435b01
Douglas Brandão <douglasfrb@gmail.com>
2024-01-12 22:46:26 -0400

    chore: notify config

--- config/nvim/lua/douglas/plugins/ui.lua
+++ config/nvim/lua/douglas/plugins/ui.lua
@@ -15,7 +15,18 @@ return {
     noice.setup({})
 
     notify.setup({
-      background_colour = "#000000"
+      background_colour = "#000000",
+      level = 2,
+      minimum_width = 50,
+      max_width = 80,
+      render = "wrapped-compact",
+      stages = "fade",
+      time_formats = {
+        notification = "%T",
+        notification_history = "%FT%T"
+      },
+      timeout = 500,
+      top_down = true
     })
 
     lualine.setup({
