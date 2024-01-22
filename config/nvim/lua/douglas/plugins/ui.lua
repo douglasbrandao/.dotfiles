251168ddaf8d6b99e76494e231bc76bf71999976
Douglas Brandão <douglasfrb@gmail.com>
2024-01-22 18:56:24 -0300

    chore: comment noice lines

--- config/nvim/lua/douglas/plugins/ui.lua
+++ config/nvim/lua/douglas/plugins/ui.lua
@@ -35,25 +35,25 @@ return {
       },
       sections = {
         lualine_x = {
-          {
-            noice.api.status.message.get_hl,
-            cond = noice.api.status.message.has,
-          },
+          -- {
+          --   noice.api.status.message.get_hl,
+          --   cond = noice.api.status.message.has,
+          -- },
           {
             noice.api.status.command.get,
             cond = noice.api.status.command.has,
             color = { fg = "#ff9e64" },
           },
-          {
-            noice.api.status.mode.get,
-            cond = noice.api.status.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            noice.api.status.search.get,
-            cond = noice.api.status.search.has,
-            color = { fg = "#ff9e64" },
-          },
+          -- {
+          --   noice.api.status.mode.get,
+          --   cond = noice.api.status.mode.has,
+          --   color = { fg = "#ff9e64" },
+          -- },
+          -- {
+          --   noice.api.status.search.get,
+          --   cond = noice.api.status.search.has,
+          --   color = { fg = "#ff9e64" },
+          -- },
         },
         lualine_c = {
           { gitblame.get_current_blame_text, cond = gitblame.is_blame_text_available }
