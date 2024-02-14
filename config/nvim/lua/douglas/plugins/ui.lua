5807055efc0b92a9a5f2a0a678f153461ef26485
Douglas Brandão <douglasfrb@gmail.com>
2024-02-14 19:34:46 -0400

    chore: catppuccin -> gruvbox

--- config/nvim/lua/douglas/plugins/ui.lua
+++ config/nvim/lua/douglas/plugins/ui.lua
@@ -39,8 +39,8 @@ return {
 
     lualine.setup({
       options = {
-        theme = 'catppuccin',
-        -- theme = 'gruvbox',
+        -- theme = 'catppuccin',
+        theme = 'gruvbox',
       },
       sections = {
         lualine_x = {
