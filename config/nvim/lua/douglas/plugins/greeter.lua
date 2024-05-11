fd982eb24c199b54e7601a6ae5bbd4ddd410ed5c
Douglas Brandão <douglasfrb@gmail.com>
2024-05-11 18:52:48 -0400

    chore: adjust greeter commands

--- config/nvim/lua/douglas/plugins/greeter.lua
+++ config/nvim/lua/douglas/plugins/greeter.lua
@@ -41,10 +41,10 @@ return {
 
     dashboard.section.buttons.val = {
       dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("f", " " .. " Find file", ":Fzf files <CR>"),
+      dashboard.button("g", "󰷾 " .. " Find text", ":Fzf live_grep <CR>"),
       dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
-      dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("r", "󰄉 " .. " Recent files", ":Fzf oldfiles <CR>"),
       dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
       dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
       dashboard.button("q", " " .. " Quit", ":qa<CR>"),
