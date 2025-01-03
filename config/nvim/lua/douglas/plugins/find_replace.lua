fa629dc167c2973a46f2ab26f008e1da5a91215b
Douglas Brandão <douglasfrb@gmail.com>
2025-01-03 18:38:59 -0400

    Revert "chore: change keymap"
    This reverts commit 360e5bafc95a86a9d70d1c51f18ec0766e2f16a7.

--- config/nvim/lua/douglas/plugins/find_replace.lua
+++ config/nvim/lua/douglas/plugins/find_replace.lua
@@ -1,20 +1,20 @@
 return {
-	"nvim-pack/nvim-spectre",
-	config = function()
-		vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
-			desc = "Toggle Spectre",
-		})
-		vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-			desc = "Search current word",
-		})
-		vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-			desc = "Search on current file",
-		})
-		vim.keymap.set("n", "<leader>R", '<cmd>lua require("spectre.actions").run_current_replace()<CR>', {
-			desc = "Replace current item",
-		})
-		vim.keymap.set("n", "<leader>RR", '<cmd>lua require("spectre.actions").run_replace()<CR>', {
-			desc = "Replace All",
-		})
-	end,
+  'nvim-pack/nvim-spectre',
+  config = function()
+    vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+      desc = "Toggle Spectre"
+    })
+    vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+      desc = "Search current word"
+    })
+    vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+      desc = "Search on current file"
+    })
+    vim.keymap.set('n', '<leader>rc', '<cmd>lua require("spectre.actions").run_current_replace()<CR>', {
+      desc = "Replace current item"
+    })
+    vim.keymap.set('n', '<leader>R', '<cmd>lua require("spectre.actions").run_replace()<CR>', {
+      desc = "Replace All"
+    })
+  end
 }
